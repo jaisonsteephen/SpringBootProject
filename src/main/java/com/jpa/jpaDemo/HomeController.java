4df19adf35dbf7996501de1fa84c6f998eee440c
jaison steephen <jaisonsteeephen@gmail.com>
2019-06-19 21:47:46 +0530

    first commit

---
+++ src/main/java/com/jpa/jpaDemo/HomeController.java
@@ -0,0 +1,13 @@
+package com.jpa.jpaDemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController; 
+
+@RestController
+public class HomeController {
+	@RequestMapping("/hello")
+	public String sayHello() {
+		return "hello";
+	}
+
+}
